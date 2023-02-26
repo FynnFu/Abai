f46d8d9ebfe7c1afd9eb673e66e8908035f96f9e
FynnFu <rasidaliev53@gmail.com>
2023-02-26 20:06:00 +0600

    ff

--- Abai/settings.py
+++ Abai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ly!yeqr9j&+4v+$-$t@!@y-1esd03eifnrerm-+qswuv=lqs95
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['arima.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
